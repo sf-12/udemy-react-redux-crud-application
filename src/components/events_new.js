026f0b68f72892c3617ac51fe230c8afc012bca4
sf-12 <s.fukasawa12@gmail.com>
2021-02-18 16:58:37 +0900

    apply material-ui to events_new and events_show components

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -2,45 +2,55 @@ import React, {Component} from "react";
 import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 import { postEvent } from '../actions'
 
 class EventsNew extends Component {
+  // 初期化
   constructor(props) {
     super(props)
     this.onSubmit = this.onSubmit.bind(this)
   }
+
+  // 入力フォーム
   renderField(field) {
     const {input, label, type, meta: {touched, error}} = field;
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
+      <TextField
+        hintText={label}
+        floatingLabelText={label}
+        type={type}
+        errorText={touched && error}
+        {...input}
+        fullWidth={true}
+      />
     )
   }
 
+  // Submit時の処理
   async onSubmit(values) {
     await this.props.postEvent(values)
     this.props.history.push('/')
   }
 
+  // 描画内容
   render() {
     const { handleSubmit, pristine, submitting, invalid } = this.props
+    const style = { margin: 12}
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
         <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
-
-        <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
-          <Link to="/" >Cancel</Link>
-        </div>
+        <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid} />
+        <RaisedButton label="Cancel" style={style} containerElement={<Link to="/" />} />
       </form>
     )
   }
 }
 
+// バリデーションチェック
 const validate = values => {
   const errors = {}
 
